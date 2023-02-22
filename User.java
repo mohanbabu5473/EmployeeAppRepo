07ce229c4bc10b9fa73bfbeb2dc69051d2d553fe
mohanbabu5473 <126046162+mohanbabu5473@users.noreply.github.com>
2023-02-22 10:30:17 +0530

    Removed override method
    Removed override method

--- User.java
+++ User.java
@@ -49,14 +49,5 @@ public class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-	@Override
-	public String toString() {
-		return "User{" +
-				"id=" + id +
-				", name='" + name + '\'' +
-				", role='" + role + '\'' +
-				", email='" + email + '\'' +
-				'}';
-	}
+	
 }
